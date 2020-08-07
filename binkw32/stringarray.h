af29dfb4f4f8d25f722e79926340e471b401557b
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2020-08-07 06:01:13 +0300

    Add StringArray structure and its api

---
+++ binkw32/stringarray.h
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the binkw32 proxy dll for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2Binkw32Proxy)
+ * Copyright (C) 2020 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef STRINGARRAY_H
+#define STRINGARRAY_H
+
+namespace game {
+
+struct String;
+
+struct StringArray
+{
+    String* begin;
+    String* end;
+    String* allocatedMemEnd;
+    void* allocator;
+};
+
+static_assert(sizeof(StringArray) == 16, "StringArray structure must be exactly 16 bytes");
+
+namespace StringArrayApi {
+
+struct Api
+{
+    /**
+     * Adds new string element to the end of the array.
+     * @param[in] thisptr pointer to array to add to.
+     * @param[in] string element to push.
+     * @returns unknown.
+     */
+    using PushBack = void*(__thiscall*)(StringArray* thisptr, const String* string);
+    PushBack pushBack;
+};
+
+/** Returns StringArray functions according to determined version of the game. */
+Api& get();
+
+} // namespace StringArrayApi
+
+} // namespace game
+
+#endif // STRINGARRAY_H
