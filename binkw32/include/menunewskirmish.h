8e32201a01eb5fc1ed11b771cdf592789d11d0ee
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2020-08-16 01:52:07 +0300

    Add CMenuNewSkirmish api functions and vftable

--- binkw32/include/menunewskirmish.h
+++ binkw32/include/menunewskirmish.h
@@ -24,6 +24,9 @@
 
 namespace game {
 
+struct String;
+struct Functor;
+
 /** Represents base class for different skirmish menus. */
 struct CMenuNewSkirmish : public CMenuBase
 { };
@@ -44,9 +47,82 @@ struct Api
                                                        int a1,
                                                        const char* dialogName);
     Constructor constructor;
+
+    /** Callback function signature used for CMenuNewSkirmish buttons. */
+    using ButtonCallback = void(__thiscall*)(CMenuNewSkirmish* thisptr);
+    ButtonCallback buttonBackCallback;
+    ButtonCallback loadScenarioCallback;
+
+    /**
+     * Creates functor for CMenuNewSkirmish buttons.
+     * @param[in] functor pointer to existing functor to initialize.
+     * @param a2 meaning unknown.
+     * @param[in] menu
+     * @param[in] pointer to button callback.
+     * @returns initialized functor.
+     */
+    using CreateButtonFunctor = Functor*(__stdcall*)(Functor* functor,
+                                                     int a2,
+                                                     CMenuNewSkirmish* menu,
+                                                     const ButtonCallback* callback);
+    CreateButtonFunctor createButtonFunctor;
+
+    /** Callback function signature used for CMenuNewSkirmish list box. */
+    using ListBoxCallback = void(__thiscall*)(CMenuNewSkirmish* thisptr, int selectedIndex);
+    ListBoxCallback listBoxCallback;
+
+    /**
+     * Creates functor for CMenuNewSkirmish list box.
+     * @param[in] functor pointer to existing functor to initialize.
+     * @param a2 meaning unknown.
+     * @param[in] menu
+     * @param[in] pointer to list box callback.
+     * @returns initialized functor.
+     */
+    using CreateListBoxFunctor = Functor*(__stdcall*)(Functor* functor,
+                                                      int a2,
+                                                      CMenuNewSkirmish* menu,
+                                                      const ListBoxCallback* callback);
+    CreateListBoxFunctor createListBoxFunctor;
+
+    /**
+     * Callback function signature used for list box display text.
+     * Used to show actual string values of list box display text ui element.
+     * @param[in] thisptr pointer to menu.
+     * @param[inout] string string to show in list box display text.
+     * @param a3 meaning unknown.
+     * @param selectedIndex index of currently selected element in list box.
+     */
+    using ListBoxDisplayTextCallback = void(__thiscall*)(CMenuNewSkirmish* thisptr,
+                                                         String* string,
+                                                         bool a3,
+                                                         int selectedIndex);
+    ListBoxDisplayTextCallback displayTextCallback;
+
+    /**
+     * Creates functor for CMenuNewSkirmish list box display text.
+     * @param[in] functor pointer to existing functor to initialize.
+     * @param a2 meaning unknown.
+     * @param[in] menu
+     * @param[in] pointer to list box display text callback.
+     * @returns initialized functor.
+     */
+    using CreateListBoxDisplayTextFunctor =
+        Functor*(__stdcall*)(Functor* functor,
+                             int a2,
+                             CMenuNewSkirmish* menu,
+                             const ListBoxDisplayTextCallback* callback);
+    CreateListBoxDisplayTextFunctor createListBoxDisplayTextFunctor;
 };
 
 Api& get();
+
+struct Vftable
+{
+    void* unknown[38];
+};
+
+Vftable* vftable();
 
 } // namespace CMenuNewSkirmishApi
 
---
+++ binkw32/src/menunewskirmish.cpp
@@ -0,0 +1,65 @@
+/*
+ * This file is part of the binkw32 proxy dll for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2Binkw32Proxy)
+ * Copyright (C) 2020 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "menunewskirmish.h"
+#include "version.h"
+#include <array>
+
+namespace game::CMenuNewSkirmishApi {
+
+// clang-format off
+static std::array<Api, 3> functions = {{
+    // Akella
+    Api{
+        (Api::Constructor)0x4e9697,
+        (Api::ButtonCallback)0x4dcf9e,
+        (Api::ButtonCallback)0x4e9d2a,
+        (Api::CreateButtonFunctor)0x4ea842,
+        (Api::ListBoxCallback)0x4e9cb3,
+        (Api::CreateListBoxFunctor)0x4ea8da,
+        (Api::ListBoxDisplayTextCallback)0x4e9c1a,
+        (Api::CreateListBoxDisplayTextFunctor)0x4ea88e
+    },
+    // Russobit
+    Api{},
+    // Gog
+    Api{}
+}};
+
+static std::array<Vftable*, 3> vftables = {{
+    // Akella
+    (Vftable*)0x6dec9c,
+    // Russobit
+    (Vftable*)0x0,
+    // Gog
+    (Vftable*)0x0
+}};
+// clang-format on
+
+Api& get()
+{
+    return functions[static_cast<int>(hooks::gameVersion())];
+}
+
+Vftable* vftable()
+{
+    return vftables[static_cast<int>(hooks::gameVersion())];
+}
+
+} // namespace game::CMenuNewSkirmishApi
