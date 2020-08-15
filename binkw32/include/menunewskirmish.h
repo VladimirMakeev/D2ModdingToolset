2f006a2ace6e4beb4dcc3ba0e244efc2a2852e9f
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2020-08-15 20:06:53 +0300

    Add CMenuNewSkirmish structure and its api

---
+++ binkw32/include/menunewskirmish.h
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the binkw32 proxy dll for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2Binkw32Proxy)
+ * Copyright (C) 2020 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MENUNEWSKIRMISH_H
+#define MENUNEWSKIRMISH_H
+
+#include "menubase.h"
+
+namespace game {
+
+/** Represents base class for different skirmish menus. */
+struct CMenuNewSkirmish : public CMenuBase
+{ };
+
+namespace CMenuNewSkirmishApi {
+
+struct Api
+{
+    /**
+     * Initializes CMenuNewSkirmish object, creates menu based on dialogName.
+     * Creates menu ui elements and assigns functors and hotkeys to them.
+     * @param[in] thisptr pointer to existing CMenuNewSkirmish object.
+     * @param a1 meaning unknown.
+     * @param[in] dialogName name of dialog to create menu and ui elements from.
+     * @returns thisptr.
+     */
+    using Constructor = CMenuNewSkirmish*(__thiscall*)(CMenuNewSkirmish* thisptr,
+                                                       int a1,
+                                                       const char* dialogName);
+    Constructor constructor;
+};
+
+Api& get();
+
+} // namespace CMenuNewSkirmishApi
+
+} // namespace game
+
+#endif // MENUNEWSKIRMISH_H
