d72e117beb55d47a6787a500c18891ff662e1d55
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2020-08-31 23:13:34 +0300

    Add CPlayerBuildings structure

---
+++ binkw32/include/playerbuildings.h
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the binkw32 proxy dll for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2Binkw32Proxy)
+ * Copyright (C) 2020 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PLAYERBUIDINGS_H
+#define PLAYERBUIDINGS_H
+
+#include "linkedlist.h"
+#include "midscenarioobject.h"
+
+namespace game {
+
+/** Holds information about player's buildings in capital. */
+struct CPlayerBuildings : public IMidScenarioObject
+{
+    int unknown;
+    int unknown2;
+    LinkedList list;
+};
+
+static_assert(sizeof(CPlayerBuildings) == 28,
+              "Size of CPlayerBuildings structure must be exactly 28 bytes");
+
+} // namespace game
+
+#endif // PLAYERBUIDINGS_H
