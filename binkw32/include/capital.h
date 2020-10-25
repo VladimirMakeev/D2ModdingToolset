950a35914a00655ac02c2b5ef312f19f58a3f3aa
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2020-10-25 07:04:53 +0300

    Add CCapital structure

---
+++ binkw32/include/capital.h
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the binkw32 proxy dll for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2Binkw32Proxy)
+ * Copyright (C) 2020 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CAPITAL_H
+#define CAPITAL_H
+
+#include "fortification.h"
+
+namespace game {
+
+/** Holds capital city related data in scenario file and game. */
+struct CCapital : public CFortification
+{ };
+
+static_assert(sizeof(CCapital) == 144, "Size of CCapital structure must be exactly 144 bytes");
+
+} // namespace game
+
+#endif // CAPITAL_H
