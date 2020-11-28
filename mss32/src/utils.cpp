4496905701b1620aada1367ee7ea954c2e010b7d
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2020-11-28 07:03:21 +0300

    Move trimSpaces to utils

---
+++ mss32/src/utils.cpp
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2020 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "utils.h"
+
+namespace hooks {
+
+std::string trimSpaces(const std::string& str)
+{
+    const auto begin = str.find_first_not_of(" ");
+    if (begin == std::string::npos) {
+        return "";
+    }
+
+    const auto end = str.find_last_not_of(" ");
+    return str.substr(begin, end - begin + 1);
+}
+
+} // namespace hooks
