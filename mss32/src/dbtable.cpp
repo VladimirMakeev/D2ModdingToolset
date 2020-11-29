9f797607d6dda2b6b27acbc80d1af341343e2745
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2020-11-30 00:19:01 +0300

    Define CDBTable functions addresses

--- mss32/src/dbtable.cpp
+++ mss32/src/dbtable.cpp
@@ -50,21 +50,60 @@ static std::array<Api, 4> functions = {{
         (Api::FindBuildingCategory)0x58c041,
         (Api::FindUnitBranchCategory)0x58c0bf,
         (Api::ReadUnitLevel)0x58be7c,
-        (Api::DuplicateRecordException)0x59d474
+        (Api::DuplicateRecordException)0x59d474,
+        (Api::ReadId)0x596935,
+        (Api::ReadText)0x596b67,
+        (Api::FindAttackClass)0x59fad5,
+        (Api::FindAttackSource)0x583f7d,
+        (Api::FindAttackReach)0x5a6a31,
+        (Api::ReadAttackIntValue)0x5a6409,
+        (Api::ReadPower)0x5a64bc,
+        (Api::ReadAttackIntValue)0x5a6505,
+        (Api::ReadAttackIntValue)0x5a6548,
+        (Api::ReadAttackLevel)0x5a657f,
+        (Api::ReadBoolValue)0x5a6624,
+        (Api::ReadIntWithBoundsCheck)0x5968cc,
+        (Api::ReadBoolValue)0x596911
     },
     // Gog
     Api{
         (Api::FindBuildingCategory)0x58b1cf,
         (Api::FindUnitBranchCategory)0x58b24d,
         (Api::ReadUnitLevel)0x58afd7,
-        (Api::DuplicateRecordException)0x59c710
+        (Api::DuplicateRecordException)0x59c710,
+        (Api::ReadId)0x595a5a,
+        (Api::ReadText)0x595c8c,
+        (Api::FindAttackClass)0x59ed57,
+        (Api::FindAttackSource)0x583144,
+        (Api::FindAttackReach)0x5a5c92,
+        (Api::ReadAttackIntValue)0x5a5655,
+        (Api::ReadPower)0x5a5708,
+        (Api::ReadAttackIntValue)0x5a5751,
+        (Api::ReadAttackIntValue)0x5a5794,
+        (Api::ReadAttackLevel)0x5a57cb,
+        (Api::ReadBoolValue)0x5a5870,
+        (Api::ReadIntWithBoundsCheck)0x5959f1,
+        (Api::ReadBoolValue)0x595a36
     },
     // Scenario Editor
     Api{
         (Api::FindBuildingCategory)0x538bf7,
         (Api::FindUnitBranchCategory)0x538c75,
         (Api::ReadUnitLevel)0x538a1b,
-        (Api::DuplicateRecordException)0x53f9b2
+        (Api::DuplicateRecordException)0x53f9b2,
+        (Api::ReadId)0x536f07,
+        (Api::ReadText)0x537139,
+        (Api::FindAttackClass)0x547d4a,
+        (Api::FindAttackSource)0x5325c0,
+        (Api::FindAttackReach)0x54ec66,
+        (Api::ReadAttackIntValue)0x54e637,
+        (Api::ReadPower)0x54e6ea,
+        (Api::ReadAttackIntValue)0x54e733,
+        (Api::ReadAttackIntValue)0x54e776,
+        (Api::ReadAttackLevel)0x54e7ad,
+        (Api::ReadBoolValue)0x54e852,
+        (Api::ReadIntWithBoundsCheck)0x536e9e,
+        (Api::ReadBoolValue)0x536ee3
     }
 }};
 // clang-format on
