69d4803f096e2e7102685a83187bd55ae19d8c91
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-01-31 18:38:12 +0300

    Add CItemPotionHeal structure

---
+++ mss32/include/itempotionheal.h
@@ -0,0 +1,51 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ITEMPOTIONHEAL_H
+#define ITEMPOTIONHEAL_H
+
+#include "itembase.h"
+#include "itemexpotionheal.h"
+#include <cstddef>
+
+namespace game {
+
+/** Represents healing potion. */
+struct CItemPotionHeal
+    : public IItemExPotionHeal
+    , public CItemBase
+{
+    int quantityHp;
+};
+
+static_assert(sizeof(CItemPotionHeal) == 20,
+              "Size of CItemPotionHeal structure must be exactly 20 bytes");
+
+static_assert(offsetof(CItemPotionHeal, CItemPotionHeal::IItemExPotionHeal::vftable) == 0,
+              "Vftable offset for IItemExPotionHeal in CItemPotionHeal structure must be 0 bytes");
+
+static_assert(offsetof(CItemPotionHeal, CItemPotionHeal::CItemBase::vftable) == 4,
+              "Vftable offset for CItemBase in CItemPotionHeal structure must be 4 bytes");
+
+static_assert(offsetof(CItemPotionHeal, quantityHp) == 16,
+              "CItemPotionHeal::quantityHp offset must be 16 bytes");
+
+} // namespace game
+
+#endif // ITEMPOTIONHEAL_H
