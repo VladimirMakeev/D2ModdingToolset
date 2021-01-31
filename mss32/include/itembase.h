845f178c26f5e06c28fea9ec7cdb961efb396141
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-01-31 17:32:32 +0300

    Add CItemBase structure

---
+++ mss32/include/itembase.h
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ITEMBASE_H
+#define ITEMBASE_H
+
+#include "currency.h"
+#include "item.h"
+#include "midgardid.h"
+#include "textandid.h"
+
+namespace game {
+
+struct CItemBaseData
+{
+    TextAndId name;
+    TextAndId description;
+    Bank value;
+    CMidgardID unitId;
+};
+
+static_assert(sizeof(CItemBaseData) == 32,
+              "Size of CItemBaseData structure must be exactly 32 bytes");
+
+/** Base class for all items read from GItem.dbf. */
+struct CItemBase : public IItem
+{
+    CMidgardID itemId;
+    CItemBaseData* data;
+};
+
+static_assert(sizeof(CItemBase) == 12, "Size of CItemBase structure must be exactly 12 bytes");
+
+} // namespace game
+
+#endif // ITEMBASE_H
