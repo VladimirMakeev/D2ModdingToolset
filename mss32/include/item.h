4e84e063bcc0fa2e776c5c28cd50761b2bda42b2
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-01-31 17:31:52 +0300

    Add IItem structure and its vftable

---
+++ mss32/include/item.h
@@ -0,0 +1,71 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ITEM_H
+#define ITEM_H
+
+namespace game {
+
+struct IItemVftable;
+struct LItemCategory;
+struct Bank;
+struct CMidgardID;
+
+/** Iterface for all items read from GItem.dbf. */
+struct IItem
+{
+    IItemVftable* vftable;
+};
+
+struct IItemVftable
+{
+    void* destructor;
+
+    /** Returns corresponding item category. */
+    using GetCategory = const LItemCategory*(__thiscall*)(const IItem* thisptr);
+    GetCategory getCategory;
+
+    using GetCString = const char*(__thiscall*)(const IItem* thisptr);
+    GetCString getName;
+    GetCString getDescription;
+
+    /** Returns item cost. */
+    using GetValue = const Bank*(__thiscall*)(const IItem* thisptr);
+    GetValue getValue;
+
+    using GetUnitId = const CMidgardID*(__thiscall*)(const IItem* thisptr);
+    GetUnitId getUnitId;
+
+    /**
+     * Tries to cast base type to derived specified by name.
+     * Return type depends on base. Should not be called directly.
+     */
+    using CastBaseToDerived = void*(__thiscall*)(const IItem* thisptr,
+                                                 const char* derivedTypeRawName);
+    CastBaseToDerived castBaseToDerived;
+
+    void* unknown;
+};
+
+static_assert(sizeof(IItemVftable) == 8 * sizeof(void*),
+              "IItem vftable must have exactly 8 methods");
+
+} // namespace game
+
+#endif // ITEM_H
