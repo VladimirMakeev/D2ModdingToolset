307ca16f7be4ccc0cbd2b2b42276a8a3b93e9d90
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-02-19 20:01:07 +0300

    Add CEncLayoutSpell api

---
+++ mss32/include/enclayoutspell.h
@@ -0,0 +1,51 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ENCLAYOUTSPELL_H
+#define ENCLAYOUTSPELL_H
+
+namespace game {
+
+struct CEncLayoutSpell;
+struct IMidgardObjectMap;
+struct CInterface;
+struct CMidgardID;
+struct CEncParamBase;
+
+namespace CEncLayoutSpellApi {
+
+struct Api
+{
+    using Constructor = CEncLayoutSpell*(__thiscall*)(CEncLayoutSpell* thisptr,
+                                                      IMidgardObjectMap* objectMap,
+                                                      CInterface* interface,
+                                                      void* a2,
+                                                      CMidgardID* spellId,
+                                                      CEncParamBase* encParam,
+                                                      CMidgardID* playerId);
+    Constructor constructor;
+};
+
+Api& get();
+
+} // namespace CEncLayoutSpellApi
+
+} // namespace game
+
+#endif // ENCLAYOUTSPELL_H
---
+++ mss32/src/enclayoutspell.cpp
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "enclayoutspell.h"
+#include "version.h"
+#include <array>
+
+namespace game::CEncLayoutSpellApi {
+
+// clang-format off
+std::array<Api, 4> functions = {{
+    // Akella
+    Api{
+        (Api::Constructor)0
+    },
+    // Russobit
+    Api{
+        (Api::Constructor)0
+    },
+    // Gog
+    Api{
+        (Api::Constructor)0
+    },
+    // Scenario Editor
+    {
+        (Api::Constructor)0x4ceb2d
+    }
+}};
+// clang-format on
+
+Api& get()
+{
+    return functions[static_cast<int>(hooks::gameVersion())];
+}
+
+} // namespace game::CEncLayoutSpellApi
