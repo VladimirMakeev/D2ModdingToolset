259b0728e4cd5bb21e85d64203d5de6ce8b0a35c
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-02-22 04:56:16 +0300

    Add CFortification vftable

--- mss32/include/fortification.h
+++ mss32/include/fortification.h
@@ -21,6 +21,7 @@
 #define FORTIFICATION_H
 
 #include "aipriority.h"
+#include "idlist.h"
 #include "mapelement.h"
 #include "midinventory.h"
 #include "midscenarioobject.h"
@@ -29,6 +30,9 @@
 #include <cstddef>
 
 namespace game {
+
+struct LFortCategory;
+struct CMidStreamFile;
 
 struct CMidUnitGroupFortification : public CMidUnitGroup
 { };
@@ -61,6 +65,67 @@ static_assert(offsetof(CFortification, group) == 60,
 
 static_assert(offsetof(CFortification, inventory) == 112,
               "CFortification::inventory offset must be 112 bytes");
+
+struct CFortificationVftable
+{
+    using Destructor = void(__thiscall*)(CFortification* thisptr, char flags);
+    Destructor destructor;
+
+    /** Serializes data to or from stream. */
+    using Stream = void*(__thiscall*)(CFortification* thisptr, int a2, int a3);
+    Stream stream;
+
+    /** Returns true if object has correct data. */
+    using IsCorrect = bool(__thiscall*)(CFortification* thisptr, IMidgardObjectMap* objectMap);
+    IsCorrect isCorrect;
+
+    using Method3 = char(__thiscall*)(CFortification* thisptr);
+    Method3 method3;
+
+    using GetCategory = const LFortCategory*(__thiscall*)(CFortification* thisptr);
+    GetCategory getCategory;
+
+    using GetCString = const char*(__thiscall*)(CFortification* thisptr);
+    GetCString getName;
+    GetCString getDescription;
+
+    /** Returns daily hp regeneration value. */
+    using GetRegen = int*(__thiscall*)(CFortification* thisptr,
+                                       int* regen,
+                                       IMidgardObjectMap* objectMap);
+    GetRegen getRegen;
+
+    using GetInt = int(__thiscall*)(CFortification* thisptr, IMidgardObjectMap* objectMap);
+    GetInt getMaxUnitsInGarrison;
+    GetInt getTier;
+
+    /** Adds protection modifier ids to the list. */
+    using GetProtection = void(__thiscall*)(CFortification* thisptr,
+                                            IMidgardObjectMap* objectMap,
+                                            IdList* list);
+    GetProtection getProtection;
+
+    using SetCString = bool(__thiscall*)(CFortification* thisptr,
+                                         int a2,
+                                         int a3,
+                                         const char* value);
+    SetCString setName;
+    SetCString setDescription;
+
+    using StreamVillageData = void(__thiscall*)(CFortification* thisptr,
+                                                int a2,
+                                                CMidStreamFile** stream);
+    StreamVillageData streamVillageData;
+
+    /** Changes fortification owner and subrace. */
+    using SetOwner = bool(__thiscall*)(CFortification* thisptr,
+                                       const CMidgardID* ownerId,
+                                       const CMidgardID* subraceId);
+    SetOwner setOwner;
+};
+
+static_assert(sizeof(CFortificationVftable) == 15 * sizeof(void*),
+              "CFortification vftable must have exactly 15 methods");
 
 } // namespace game
 
