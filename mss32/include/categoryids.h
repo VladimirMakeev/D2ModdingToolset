b5fb8c74db8b4ab79a596145f6e71d615f8aac66
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-03-14 15:49:40 +0300

    Add LSiteCategory and its table

--- mss32/include/categoryids.h
+++ mss32/include/categoryids.h
@@ -230,6 +230,15 @@ enum class FortId : int
     Village = 4
 };
 
+/** Site ids from LSite.dbf. */
+enum class SiteId : int
+{
+    Merchant = 2,
+    MageTower = 4,
+    Mercenaries,
+    Trainer
+};
+
 } // namespace game
 
 #endif // CATEGORYIDS_H
