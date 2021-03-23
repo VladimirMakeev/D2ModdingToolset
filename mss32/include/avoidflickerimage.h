df5316f5f97dbd7d4c5d05c2e702e5303e117549
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-03-24 01:32:37 +0300

    Add CAvoidFlickerImage structure

---
+++ mss32/include/avoidflickerimage.h
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef AVOIDFLICKERIMAGE_H
+#define AVOIDFLICKERIMAGE_H
+
+#include "mqimage2surface16.h"
+
+namespace game {
+
+// Actual size unknown
+struct CAvoidFlickerImage : public CMqImage2Surface16
+{ };
+
+} // namespace game
+
+#endif // AVOIDFLICKERIMAGE_H
