3c3bcf41dd1a4b69c153c626e4ef81a8d9c3777d
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-03-24 01:33:31 +0300

    Add CBatViewerUtils structure

---
+++ mss32/include/batviewerutils.h
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BATVIEWERUTILS_H
+#define BATVIEWERUTILS_H
+
+namespace game {
+
+struct CBatViewerUtils
+{
+    struct CAnimCounter
+    {
+        void* vftable;
+        void* data;
+    };
+};
+
+} // namespace game
+
+#endif // BATVIEWERUTILS_H
