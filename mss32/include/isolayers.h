6bba8f120fbfdd1479234b346ec5982d50538a30
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-04-13 00:32:29 +0300

    Add iso layers api

---
+++ mss32/include/isolayers.h
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ISOLAYER_H
+#define ISOLAYER_H
+
+namespace game {
+
+/** Addresses of layer numbers read from IIsoLay.dbf. */
+struct IsoLayers
+{
+    int* symMovePath;
+};
+
+IsoLayers& isoLayers();
+
+} // namespace game
+
+#endif // ISOLAYER_H
