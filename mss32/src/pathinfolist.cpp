20e95053e5b0a8823a8753f3cfb2e1a400e1d88b
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-04-13 00:58:01 +0300

    Add PathInfoList and its api

---
+++ mss32/src/pathinfolist.cpp
@@ -0,0 +1,57 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "pathinfolist.h"
+#include "version.h"
+#include <array>
+
+namespace game::PathInfoListApi {
+
+// clang-format off
+static std::array<Api, 3> functions = {{
+    // Akella
+    Api{
+        (Api::Constructor)0x4cdc3b,
+        (Api::PopulateFromPath)0x4cd9df,
+        (Api::FreeNodes)0x4cdca8,
+        (Api::FreeNode)0x4c8755,
+    },
+    // Russobit
+    Api{
+        (Api::Constructor)0x4cdc3b,
+        (Api::PopulateFromPath)0x4cd9df,
+        (Api::FreeNodes)0x4cdca8,
+        (Api::FreeNode)0x4c8755,
+    },
+    // Gog
+    Api{
+        (Api::Constructor)0x4cd334,
+        (Api::PopulateFromPath)0x4cd0d8,
+        (Api::FreeNodes)0x44df27,
+        (Api::FreeNode)0x5215fc,
+    }
+}};
+// clang-format on
+
+Api& get()
+{
+    return functions[static_cast<int>(hooks::gameVersion())];
+}
+
+} // namespace game::PathInfoListApi
