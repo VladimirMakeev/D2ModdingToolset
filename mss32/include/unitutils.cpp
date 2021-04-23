d94ab17ea7aa3e90dc519c4594d74955a0513a7a
Stanislav Egorov <unven@mail.ru>
2021-04-24 01:16:33 +0300

    Add unit utility functions

---
+++ mss32/include/unitutils.cpp
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Stanislav Egorov.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "unitutils.h"
+#include "globaldata.h"
+#include "midgardid.h"
+#include "unitgenerator.h"
+
+namespace hooks {
+
+void generateUnitImplByAttackId(const game::CMidgardID* attackId)
+{
+    using namespace game;
+
+    const auto& id = CMidgardIDApi::get();
+    int typeIndex = id.getTypeIndex(attackId);
+    int categoryIndex = id.getCategoryIndex(attackId);
+    IdCategory category = id.getCategory(attackId);
+
+    CMidgardID unitImplId{};
+    id.fromParts(&unitImplId, category, categoryIndex, IdType::UnitGenerated, typeIndex);
+
+    CUnitGenerator* unitGenerator = (*(GlobalDataApi::get().getGlobalData()))->unitGenerator;
+    unitGenerator->vftable->generateUnitImpl(unitGenerator, &unitImplId);
+}
+
+} // namespace hooks
