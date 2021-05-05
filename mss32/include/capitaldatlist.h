86f7c392e3ba2154c48cf900f8fe54614ac0690b
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-05-06 01:58:45 +0300

    Add CapitalDatList and its api

---
+++ mss32/include/capitaldatlist.h
@@ -0,0 +1,54 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CAPITALDATLIST_H
+#define CAPITALDATLIST_H
+
+#include "linkedlist.h"
+#include "midgardid.h"
+
+namespace game {
+
+struct CapitalDatRecord
+{
+    CMidgardID buildingId;
+    CMidgardID unknownId;
+    LinkedList<void*> list;
+};
+
+static_assert(sizeof(CapitalDatRecord) == 24,
+              "Size of CapitalDatRecord structure must be exactly 24 bytes");
+
+using CapitalDatList = LinkedList<CapitalDatRecord**>;
+
+namespace CapitalDatListApi {
+
+struct Api
+{
+    using Constructor = CapitalDatList*(__thiscall*)(CapitalDatList* thisptr);
+    Constructor constructor;
+};
+
+Api& get();
+
+} // namespace CapitalDatListApi
+
+} // namespace game
+
+#endif // CAPITALDATLIST_H
