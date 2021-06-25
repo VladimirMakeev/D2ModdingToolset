dd24c323719b7450f64565f0e32ebde1490b4ee6
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-06-25 21:22:04 +0300

    Define CPictureInterf members

---
+++ mss32/include/functordispatch2.h
@@ -0,0 +1,47 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FUNCTORDISPATCH2_H
+#define FUNCTORDISPATCH2_H
+
+namespace game {
+
+template <typename T, typename U>
+struct CBFunctorDispatch2Vftable;
+
+template <typename T, typename U>
+struct CBFunctorDispatch2
+{
+    CBFunctorDispatch2Vftable<T, U>* vftable;
+};
+
+template <typename T, typename U>
+struct CBFunctorDispatch2Vftable
+{
+    /** Calls functor-specific callback function, passing it T and U as a parameters. */
+    using RunCallback = void(__thiscall*)(CBFunctorDispatch2<T, U>* thistr, T value, U value2);
+    RunCallback runCallback;
+};
+
+static_assert(sizeof(CBFunctorDispatch2Vftable<void*, void*>) == 1 * sizeof(void*),
+              "CBFunctorDispatch2 vftable must have exactly 1 method");
+
+} // namespace game
+
+#endif // FUNCTORDISPATCH2_H
