7138d548fc57a6a53f60dd50e308d0e4138def0d
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-08-11 03:34:03 +0300

    Add ITestCondition structure and its api

---
+++ mss32/include/testcondition.h
@@ -0,0 +1,69 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TESTCONDITION_H
+#define TESTCONDITION_H
+
+namespace game {
+
+struct IMidgardObjectMap;
+struct CMidgardID;
+struct CMidEvCondition;
+struct ITestConditionVftable;
+
+/** Event condition testing interface. */
+struct ITestCondition
+{
+    ITestConditionVftable* vftable;
+};
+
+struct ITestConditionVftable
+{
+    using Destructor = void(__thiscall*)(ITestCondition* thisptr, char flags);
+    Destructor destructor;
+
+    /** Returns true if event condition meets for specified player. */
+    using Test = bool(__thiscall*)(const ITestCondition* thisptr,
+                                   const IMidgardObjectMap* objectMap,
+                                   const CMidgardID* playerId,
+                                   const CMidgardID* eventId);
+    Test test;
+};
+
+static_assert(sizeof(ITestConditionVftable) == 2 * sizeof(void*),
+              "ITestCondition vftable must have exactly 2 methods");
+
+namespace ITestConditionApi {
+
+struct Api
+{
+    /** Creates test condition object from specified event condition. */
+    using Create = ITestCondition*(__stdcall*)(CMidEvCondition* eventCondition,
+                                               bool samePlayer,
+                                               const CMidgardID* triggererStackId);
+    Create create;
+};
+
+Api& get();
+
+} // namespace ITestConditionApi
+
+} // namespace game
+
+#endif // TESTCONDITION_H
