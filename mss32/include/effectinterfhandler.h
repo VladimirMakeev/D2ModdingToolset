57f44f71a3e3739726a29754a3acf8de7fc0c895
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-08-20 01:27:58 +0300

    Add CEffectInterfHandler structure

---
+++ mss32/include/effectinterfhandler.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EFFECTINTERFHANDLER_H
+#define EFFECTINTERFHANDLER_H
+
+namespace game::editor {
+
+struct CEffectInterfHandlerVftable;
+
+struct CEffectInterfHandler
+{
+    CEffectInterfHandlerVftable* vftable;
+};
+
+struct CEffectInterfHandlerVftable
+{
+    using Destructor = void(__thiscall*)(CEffectInterfHandler* thisptr, char flags);
+    Destructor destructor;
+
+    using RunCallback = void(__thiscall*)(CEffectInterfHandler* thisptr, bool);
+    RunCallback runCallback;
+};
+
+} // namespace game::editor
+
+#endif // EFFECTINTERFHANDLER_H
