4e96291c78685fa648d3d01526f004ed2f59ec2a
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-09-12 01:08:53 +0300

    Add lua binding for IMidgardObjectMap

---
+++ mss32/include/bindings/scenarioview.h
@@ -0,0 +1,57 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SCENARIOVIEW_H
+#define SCENARIOVIEW_H
+
+#include <optional>
+#include <string>
+
+namespace sol {
+class state;
+}
+
+namespace game {
+struct IMidgardObjectMap;
+}
+
+namespace bindings {
+
+struct IdView;
+class LocationView;
+
+class ScenarioView
+{
+public:
+    ScenarioView(const game::IMidgardObjectMap* objectMap);
+
+    static void bind(sol::state& lua);
+
+    /** Searches for location by id string. */
+    std::optional<LocationView> getLocation(const std::string& id) const;
+    /** Searches for location by id. */
+    std::optional<LocationView> getLocationById(const IdView& id) const;
+
+private:
+    const game::IMidgardObjectMap* objectMap;
+};
+
+} // namespace bindings
+
+#endif // SCENARIOVIEW_H
---
+++ mss32/src/bindings/scenarioview.cpp
@@ -0,0 +1,65 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "scenarioview.h"
+#include "dynamiccast.h"
+#include "locationview.h"
+#include "midgardobjectmap.h"
+#include <sol/sol.hpp>
+
+namespace bindings {
+
+ScenarioView::ScenarioView(const game::IMidgardObjectMap* objectMap)
+    : objectMap(objectMap)
+{ }
+
+void ScenarioView::bind(sol::state& lua)
+{
+    auto scenario = lua.new_usertype<ScenarioView>("Scenario");
+    scenario["getLocation"] = sol::overload<>(&ScenarioView::getLocation,
+                                              &ScenarioView::getLocationById);
+}
+
+std::optional<LocationView> ScenarioView::getLocation(const std::string& id) const
+{
+    return getLocationById(IdView{id});
+}
+
+std::optional<LocationView> ScenarioView::getLocationById(const IdView& id) const
+{
+    using namespace game;
+
+    auto obj = objectMap->vftable->findScenarioObjectById(objectMap, &id.id);
+    if (!obj) {
+        return std::nullopt;
+    }
+
+    const auto dynamicCast = RttiApi::get().dynamicCast;
+    const auto& rtti = RttiApi::rtti();
+
+    auto* location = (const game::CMidLocation*)dynamicCast(obj, 0, rtti.IMidScenarioObjectType,
+                                                            rtti.CMidLocationType, 0);
+    if (!location) {
+        return std::nullopt;
+    }
+
+    return LocationView{location};
+}
+
+} // namespace bindings
