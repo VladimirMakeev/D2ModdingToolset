d1300021685de1bbb7a7fa71ad6c96688080f03c
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-09-14 02:23:52 +0300

    Add IMqNetSession vftable

--- mss32/include/mqnetsession.h
+++ mss32/include/mqnetsession.h
@@ -20,12 +20,56 @@
 #ifndef MQNETSESSION_H
 #define MQNETSESSION_H
 
+#include "linkedlist.h"
+
 namespace game {
+
+struct String;
+struct IMqNetPlayerEnum;
+struct IMqNetPlayerClient;
+struct IMqNetPlayerServer;
+struct IMqNetReception;
+struct IMqNetSessionVftable;
 
 struct IMqNetSession
 {
-    void* vftable;
+    IMqNetSessionVftable* vftable;
 };
+
+struct IMqNetSessionVftable
+{
+    using Destructor = void(__thiscall*)(IMqNetSession* thisptr, char flags);
+    Destructor destructor;
+
+    using Method1 = String*(__thiscall*)(IMqNetSession* thisptr, String* string);
+    Method1 method1;
+
+    using Method2 = int(__thiscall*)(IMqNetSession* thisptr);
+    Method2 method2;
+
+    using Method3 = bool(__thiscall*)(IMqNetSession* thisptr);
+    Method3 method3;
+
+    using GetPlayers = void(__thiscall*)(IMqNetSession* thisptr,
+                                         LinkedList<IMqNetPlayerEnum*>* players);
+    GetPlayers getPlayers;
+
+    using CreateClient = void(__thiscall*)(IMqNetSession* thisptr,
+                                           IMqNetPlayerClient** client,
+                                           int a3,
+                                           IMqNetReception* reception,
+                                           const char* clientName);
+    CreateClient createClient;
+
+    using CreateServer = void(__thiscall*)(IMqNetSession* thisptr,
+                                           IMqNetPlayerServer** server,
+                                           int a3,
+                                           int a4);
+    CreateServer createServer;
+};
+
+static_assert(sizeof(IMqNetSessionVftable) == 7 * sizeof(void*),
+              "IMqNetSession vftable must have exactly 7 methods");
 
 } // namespace game
 
