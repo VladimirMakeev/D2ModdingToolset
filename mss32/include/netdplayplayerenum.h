b447be414ae9e899fdf6cd45f7b203f967f1ee90
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-09-29 01:39:52 +0300

    Add CNetDPlayPlayerEnum structure

---
+++ mss32/include/netdplayplayerenum.h
@@ -0,0 +1,39 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NETDPLAYPLAYERENUM_H
+#define NETDPLAYPLAYERENUM_H
+
+#include "d2string.h"
+#include "mqnetplayerenum.h"
+
+namespace game {
+
+struct CNetDPlayPlayerEnum : public IMqNetPlayerEnum
+{
+    int playerNetId;
+    String name;
+};
+
+static_assert(sizeof(CNetDPlayPlayerEnum) == 24,
+              "Size of CNetDPlayPlayerEnum structure must be exactly 24 bytes");
+
+} // namespace game
+
+#endif // NETDPLAYPLAYERENUM_H
