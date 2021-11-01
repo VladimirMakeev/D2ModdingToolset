17927eeca0a90750b4dda126ce949cb44314f50d
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-11-02 00:42:54 +0300

    Add CBFunctorDispatch0 and CBFunctorDispatch0wRet structures

---
+++ mss32/include/functordispatch0.h
@@ -0,0 +1,63 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FUNCTORDISPATCH0_H
+#define FUNCTORDISPATCH0_H
+
+namespace game {
+
+struct CBFunctorDispatch0Vftable;
+
+struct CBFunctorDispatch0
+{
+    CBFunctorDispatch0Vftable* vftable;
+};
+
+struct CBFunctorDispatch0Vftable
+{
+    using RunCallback = void(__thiscall*)(CBFunctorDispatch0* thisptr);
+    RunCallback runCallback;
+};
+
+static_assert(sizeof(CBFunctorDispatch0Vftable) == 1 * sizeof(void*),
+              "CBFunctorDispatch0 vftable must have exactly 1 method");
+
+template <typename T>
+struct CBFunctorDispatch0wRetVftable;
+
+template <typename T>
+struct CBFunctorDispatch0wRet
+{
+    CBFunctorDispatch0wRetVftable<T>* vftable;
+};
+
+template <typename T>
+struct CBFunctorDispatch0wRetVftable
+{
+    /** Calls functor-specific callback function that returns T. */
+    using RunCallback = T(__thiscall*)(CBFunctorDispatch0wRet<T>* thistr);
+    RunCallback runCallback;
+};
+
+static_assert(sizeof(CBFunctorDispatch0wRetVftable<void*>) == 1 * sizeof(void*),
+              "CBFunctorDispatch0wRet vftable must have exactly 1 method");
+
+} // namespace game
+
+#endif // FUNCTORDISPATCH0_H
