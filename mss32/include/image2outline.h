b1967965d5d7868c7d98b23e4219873b556d6c38
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-11-22 00:34:50 +0300

    Add CImage2Outline structure and its api

---
+++ mss32/include/image2outline.h
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef IMAGE2OUTLINE_H
+#define IMAGE2OUTLINE_H
+
+#include "d2color.h"
+#include "mqimage2surface16.h"
+
+namespace game {
+
+/** Renders thin rectangle outline. */
+struct CImage2Outline : public CMqImage2Surface16
+{
+    Color color;
+    CMqPoint size2;
+};
+
+static_assert(sizeof(CImage2Outline) == 56,
+              "Size of CImage2Outline structure must be exactly 56 bytes");
+
+namespace CImage2OutlineApi {
+
+struct Api
+{
+    using Constructor = CImage2Outline*(__thiscall*)(CImage2Outline* thisptr,
+                                                     const CMqPoint* size,
+                                                     const Color* color,
+                                                     std::uint32_t opacity);
+    Constructor constructor;
+};
+
+Api& get();
+
+} // namespace CImage2OutlineApi
+
+} // namespace game
+
+#endif // IMAGE2OUTLINE_H
---
+++ mss32/src/image2outline.cpp
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "image2outline.h"
+#include "version.h"
+#include <array>
+
+namespace game::CImage2OutlineApi {
+
+// clang-format off
+static std::array<Api, 4> functions = {{
+    // Akella
+    Api{
+        (Api::Constructor)0x53abbe,
+    },
+    // Russobit
+    Api{
+        (Api::Constructor)0x53abbe,
+    },
+    // Gog
+    Api{
+        (Api::Constructor)0x53a25f,
+    },
+    // Scenario Editor
+    Api{
+        (Api::Constructor)0,
+    },
+}};
+// clang-format on
+
+Api& get()
+{
+    return functions[static_cast<int>(hooks::gameVersion())];
+}
+
+} // namespace game::CImage2OutlineApi
