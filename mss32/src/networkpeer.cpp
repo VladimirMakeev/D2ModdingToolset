5478415ab9ff28ada21877649e79dc43d4708fce
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2021-12-30 01:12:52 +0300

    Add NetworkPeer structure

---
+++ mss32/src/networkpeer.cpp
@@ -0,0 +1,64 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2021 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "networkpeer.h"
+#include "utils.h"
+#include <algorithm>
+
+namespace hooks {
+
+void __fastcall packetEventCallback(NetworkPeer* netPeer, int /*%edx*/)
+{
+    auto peer{netPeer->peer.get()};
+
+    for (auto packet = peer->Receive(); packet != nullptr;
+         peer->DeallocatePacket(packet), packet = peer->Receive()) {
+
+        auto type = static_cast<DefaultMessageIDTypes>(packet->data[0]);
+
+        for (auto& callback : netPeer->callbacks) {
+            callback->onPacketReceived(type, peer, packet);
+        }
+    }
+}
+
+NetworkPeer::NetworkPeer(PeerPtr&& peer)
+    : peer{std::move(peer)}
+{
+    createTimerEvent(&packetEvent, this, packetEventCallback, 100);
+}
+
+NetworkPeer::~NetworkPeer()
+{
+    game::UiEventApi::get().destructor(&packetEvent);
+}
+
+void NetworkPeer::addCallback(NetworkPeerCallbacks* callback)
+{
+    if (std::find(callbacks.begin(), callbacks.end(), callback) == callbacks.end()) {
+        callbacks.push_back(callback);
+    }
+}
+
+void NetworkPeer::removeCallback(NetworkPeerCallbacks* callback)
+{
+    callbacks.erase(std::remove(callbacks.begin(), callbacks.end(), callback), callbacks.end());
+}
+
+} // namespace hooks
