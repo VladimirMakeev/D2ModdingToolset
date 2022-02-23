a9c3e8fff9af21674100a4ca4eceb15a95b07005
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2022-02-23 16:35:35 +0300

    Add ICanSelect structure and its vftable

---
+++ mss32/include/canselect.h
@@ -0,0 +1,47 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2022 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CANSELECT_H
+#define CANSELECT_H
+
+namespace game {
+
+struct CMqPoint;
+struct ICanSelectVftable;
+
+struct ICanSelect
+{
+    ICanSelectVftable* vftable;
+};
+
+struct ICanSelectVftable
+{
+    using Destructor = void(__thiscall*)(ICanSelect* thisptr, char flags);
+    Destructor destructor;
+
+    using CanSelect = bool(__thiscall*)(ICanSelect* thisptr, const CMqPoint* mapPosition);
+    CanSelect canSelect;
+};
+
+static_assert(sizeof(ICanSelectVftable) == 2 * sizeof(void*),
+              "ICanSelect vftable must have exactly 2 methods");
+
+} // namespace game
+
+#endif // CANSELECT_H
