03ad38d7ddfb8e08e226ecbf4e985f5dcf81f1be
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2022-03-11 17:48:18 +0300

    Define display handler callback for CMidVillage

---
+++ mss32/src/displayhandlers.cpp
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2022 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "displayhandlers.h"
+#include "version.h"
+#include <array>
+
+namespace game::DisplayHandlersApi {
+
+// clang-format off
+static std::array<Api, 4> functions = {{
+    // Akella
+    Api{
+        (Api::DisplayHandler<CMidVillage>)0x5bcb73,
+    },
+    // Russobit
+    Api{
+        (Api::DisplayHandler<CMidVillage>)0x5bcb73,
+    },
+    // Gog
+    Api{
+        (Api::DisplayHandler<CMidVillage>)0x5bbc37,
+    },
+    // Scenario Editor
+    Api{
+        (Api::DisplayHandler<CMidVillage>)0x55d9eb,
+    },
+}};
+// clang-format on
+
+Api& get()
+{
+    return functions[static_cast<int>(hooks::gameVersion())];
+}
+
+} // namespace game::DisplayHandlersApi
