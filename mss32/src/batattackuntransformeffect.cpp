2128c0d54499586e1b56e189c851f0e447459243
Stanislav Egorov <unven@mail.ru>
2022-03-19 12:15:52 +0300

    Describe CBatAttackUntransformEffect

---
+++ mss32/src/batattackuntransformeffect.cpp
@@ -0,0 +1,44 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2022 Stanislav Egorov.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "batattackuntransformeffect.h"
+#include "version.h"
+#include <array>
+
+namespace game::CBatAttackUntransformEffectApi {
+
+// clang-format off
+static std::array<IBatAttackVftable*, 4> vftables = {{
+    // Akella
+    (IBatAttackVftable*)0x6f5624,
+    // Russobit
+    (IBatAttackVftable*)0x6f5624,
+    // Gog
+    (IBatAttackVftable*)0x6f35d4,
+    // Scenario Editor
+    (IBatAttackVftable*)nullptr,
+}};
+// clang-format on
+
+IBatAttackVftable* vftable()
+{
+    return vftables[static_cast<int>(hooks::gameVersion())];
+}
+
+} // namespace game::CBatAttackUntransformEffectApi
