ac180a1cc0cef98e3ca23eaf58437ee5ca44c925
Stanislav Egorov <unven@mail.ru>
2022-05-12 14:25:24 +0300

    Update CBattleViewerInterfData fields

--- mss32/include/battleviewerinterf.h
+++ mss32/include/battleviewerinterf.h
@@ -196,8 +196,10 @@ struct CBattleViewerInterfData
     CBattleViewerUnknown unknown11;
     char unknown5[4];
     CBatViewerUtils::CAnimCounter animCounter;
-    char unknown10[4];
-    CMqRect pUnitGroupSpotArea;
+    char unknown10;
+    bool unknown12;
+    char unknown13[2];
+    CMqRect unitGroupSpotArea;
     bool unknown7;
     bool unknown8;
     bool unknown9;
