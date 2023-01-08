a5676f84b9ab7acab6ec84bcb9ab49fa8712672c
Stanislav Egorov <unven@mail.ru>
2023-01-09 01:29:46 +0300

    Define CRefreshInfo and its api

---
+++ mss32/include/refreshinfo.h
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2023 Stanislav Egorov.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef REFRESHINFO_H
+#define REFRESHINFO_H
+
+#include "d2assert.h"
+#include "netmsg.h"
+
+namespace game {
+
+struct CMidgardID;
+struct CMidgardScenarioMap;
+
+struct CRefreshInfo : CNetMsg
+{
+    CMidgardScenarioMap* scenarioMap;
+    bool unknown;
+};
+
+assert_size(CRefreshInfo, 12);
+
+namespace CRefreshInfoApi {
+
+struct Api
+{
+    using Constructor = CRefreshInfo*(__thiscall*)(CRefreshInfo* thisptr);
+    Constructor constructor;
+
+    using Constructor2 = CRefreshInfo*(__thiscall*)(CRefreshInfo* thisptr,
+                                                    CMidgardID* scenarioFileId,
+                                                    bool a3);
+    Constructor2 constructor2;
+
+    using Destructor = void(__thiscall*)(CRefreshInfo* thisptr);
+    Destructor destructor;
+
+    using AddObject = bool(__thiscall*)(CRefreshInfo* thisptr,
+                                        const CMidgardScenarioMap* scenarioMap,
+                                        const CMidgardID* objectId);
+    AddObject addObject;
+};
+
+Api& get();
+
+CNetMsgVftable* vftable();
+
+} // namespace CRefreshInfoApi
+
+} // namespace game
+
+#endif // REFRESHINFO_H
---
+++ mss32/src/refreshinfo.cpp
@@ -0,0 +1,82 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2023 Stanislav Egorov.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "refreshinfo.h"
+#include "version.h"
+#include <array>
+
+namespace game::CRefreshInfoApi {
+
+// clang-format off
+static std::array<Api, 4> functions = {{
+    // Akella
+    Api{
+        (Api::Constructor)0x47c3c7,
+        (Api::Constructor2)0x47c3fd,
+        (Api::Destructor)0x415c08,
+        (Api::AddObject)0x47c471,
+    },
+    // Russobit
+    Api{
+        (Api::Constructor)0x47c3c7,
+        (Api::Constructor2)0x47c3fd,
+        (Api::Destructor)0x415c08,
+        (Api::AddObject)0x47c471,
+    },
+    // Gog
+    Api{
+        (Api::Constructor)0x47bf45,
+        (Api::Constructor2)0x47bf7b,
+        (Api::Destructor)0x4158ee,
+        (Api::AddObject)0x47bfef,
+    },
+    // Scenario Editor
+    Api{
+        (Api::Constructor)nullptr,
+        (Api::Constructor2)nullptr,
+        (Api::Destructor)nullptr,
+        (Api::AddObject)nullptr,
+    },
+}};
+// clang-format on
+
+Api& get()
+{
+    return functions[static_cast<int>(hooks::gameVersion())];
+}
+
+// clang-format off
+static std::array<CNetMsgVftable*, 4> vftables = {{
+    // Akella
+    (CNetMsgVftable*)0x6d4f64,
+    // Russobit
+    (CNetMsgVftable*)0x6d4f64,
+    // Gog
+    (CNetMsgVftable*)0x6d2f04,
+    // Scenario Editor
+    (CNetMsgVftable*)nullptr,
+}};
+// clang-format on
+
+CNetMsgVftable* vftable()
+{
+    return vftables[static_cast<int>(hooks::gameVersion())];
+}
+
+} // namespace game::CRefreshInfoApi
