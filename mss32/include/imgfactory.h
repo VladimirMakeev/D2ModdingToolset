ab6663e25af86bc34f6a9d2b59ba997d0d901ab4
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2023-02-07 20:57:42 +0300

    Define CImgFactory subclasses

--- mss32/include/imgfactory.h
+++ mss32/include/imgfactory.h
@@ -20,29 +20,89 @@
 #ifndef IMGFACTORY_H
 #define IMGFACTORY_H
 
+#include "d2string.h"
+#include "d2vector.h"
 #include "mqimage2.h"
+#include "smartptr.h"
 
 namespace game {
 
 struct CImgFactory
 {
+    struct ImageVftable;
+
     struct Image
     {
-        struct Vftable : public IMqImage2Vftable
-        {
-            using GetImage = IMqImage2*(__thiscall*)(const Image* thisptr,
-                                                     std::uint32_t imageIndex);
-            GetImage getImage;
+        ImageVftable* vftable;
+    };
 
-            using IsUnknownReset = bool(__thiscall*)(const Image* thisptr);
-            IsUnknownReset isUnknownReset;
-        };
+    struct IdManagerVftable;
 
-        Vftable* vftable;
+    struct IdManager
+    {
+        IdManagerVftable* vftable;
+        int value;
+    };
+
+    struct FactoryLoaderVftable;
+
+    struct FactoryLoader
+    {
+        FactoryLoaderVftable* vftable;
     };
 };
 
-assert_vftable_size(CImgFactory::Image::Vftable, 11);
+using IdManagerSmartPtr = SmartPtr<CImgFactory::IdManager>;
+
+struct CImgFactory::ImageVftable : public IMqImage2Vftable
+{
+    using GetImage = IMqImage2*(__thiscall*)(const Image* thisptr, std::uint32_t imageIndex);
+    GetImage getImage;
+
+    using IsUnknownReset = bool(__thiscall*)(const Image* thisptr);
+    IsUnknownReset isUnknownReset;
+};
+
+assert_vftable_size(CImgFactory::ImageVftable, 11);
+
+struct CImgFactory::IdManagerVftable
+{
+    using Destructor = void(__thiscall*)(CImgFactory::IdManager* thisptr, char flags);
+    Destructor destructor;
+
+    /** Returns sum of IdManager::value and a2. */
+    using Method1 = int(__thiscall*)(CImgFactory::IdManager* thisptr, int a2);
+    Method1 method1;
+
+    /**
+     * Returns difference between a2 and IdManager::value if a2 is greater.
+     * Otherwise, returns a2 as is.
+     */
+    using Method2 = int(__thiscall*)(CImgFactory::IdManager* thisptr, int a2);
+    Method2 method2;
+};
+
+assert_vftable_size(CImgFactory::IdManagerVftable, 3);
+
+struct CImgFactory::FactoryLoaderVftable
+{
+    using Destructor = void(__thiscall*)(CImgFactory::FactoryLoader* thisptr, char flags);
+    Destructor destructor;
+
+    using LoadImageNames = void(__thiscall*)(CImgFactory::FactoryLoader* thisptr,
+                                             const char* imageName,
+                                             Vector<String>* imageNames);
+    LoadImageNames loadImageNames;
+};
+
+assert_vftable_size(CImgFactory::FactoryLoaderVftable, 2);
+
+struct CImgFactoryAnimLoaderWeakDup : public CImgFactory::FactoryLoader
+{
+    CImgFactory::FactoryLoader* factoryLoader;
+};
+
+assert_size(CImgFactoryAnimLoaderWeakDup, 8);
 
 } // namespace game
 
