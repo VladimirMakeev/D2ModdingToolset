0a8d55223f0a508b4e5e0469c173553fff42eb3b
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2023-02-07 20:59:11 +0300

    Add FactoryImageStill structure

---
+++ mss32/include/factoryimagestill.h
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2023 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FACTORYIMAGESTILL_H
+#define FACTORYIMAGESTILL_H
+
+#include "imgfactory.h"
+
+namespace game {
+
+struct FactoryImage2;
+
+struct FactoryImageStill : public CImgFactory::Image
+{
+    FactoryImage2* image;
+};
+
+assert_size(FactoryImageStill, 8);
+
+} // namespace game
+
+#endif // FACTORYIMAGESTILL_H
