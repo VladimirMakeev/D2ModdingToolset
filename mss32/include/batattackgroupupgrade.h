1e28bf7815af57ccdbb2a83319ae786663ca3442
Stanislav Egorov <unven@mail.ru>
2023-02-25 22:58:39 +0300

    Add CBatAttackGroupUpgrade api

---
+++ mss32/include/batattackgroupupgrade.h
@@ -0,0 +1,48 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2023 Stanislav Egorov.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BATATTACKGROUPUPGRADE_H
+#define BATATTACKGROUPUPGRADE_H
+
+namespace game {
+
+struct CMidgardID;
+struct IMidgardObjectMap;
+struct BattleMsgData;
+struct BattleAttackInfo;
+
+namespace CBatAttackGroupUpgradeApi {
+
+struct Api
+{
+    using UpgradeGroup = void(__stdcall*)(game::IMidgardObjectMap* objectMap,
+                                          game::BattleMsgData* battleMsgData,
+                                          const game::CMidgardID* groupId,
+                                          game::BattleAttackInfo** attackInfo,
+                                          bool playableRaceAi);
+    UpgradeGroup upgradeGroup;
+};
+
+Api& get();
+
+} // namespace CBatAttackGroupUpgradeApi
+
+} // namespace game
+
+#endif // BATATTACKGROUPUPGRADE_H
