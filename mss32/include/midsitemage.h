25a92bedb82ad385cc5c7d938a881b72033090f9
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2024-03-13 21:50:37 +0300

    Add CMidSiteMage structure

---
+++ mss32/include/midsitemage.h
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2024 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MIDSITEMAGE_H
+#define MIDSITEMAGE_H
+
+#include "idlist.h"
+#include "midsite.h"
+
+namespace game {
+
+/** Holds mage tower related data in scenario file and game. */
+struct CMidSiteMage : public CMidSite
+{
+    IdList spells;
+};
+
+assert_size(CMidSiteMage, 136);
+
+} // namespace game
+
+#endif // MIDSITEMAGE_H
