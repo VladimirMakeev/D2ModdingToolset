aab0d6ee1588a12f5b2bdc5b4639bc60fd9f6ba1
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2024-03-16 15:46:58 +0300

    Add CScenPropInterf structure and its api

---
+++ mss32/src/scenpropinterf.cpp
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the modding toolset for Disciples 2.
+ * (https://github.com/VladimirMakeev/D2ModdingToolset)
+ * Copyright (C) 2024 Vladimir Makeev.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "scenpropinterf.h"
+
+namespace game::editor::CScenPropInterfApi {
+
+Api& get()
+{
+    static Api api{
+        (Api::Constructor)0x47396A,
+        (Api::CreateSpinButtonFunctor)0x4441b1,
+    };
+
+    return api;
+}
+
+} // namespace game::editor::CScenPropInterfApi
